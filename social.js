3cdaec0a253ae7b6a068e881820096539380a849
Dylan Luchmun <dylan@MacBook-Pro-Dylan.local>
2022-05-24 14:55:06 +0200

    first commit

---
+++ social.js
@@ -0,0 +1,61 @@
+(function() {
+    var popupCenter = function(url, title, width, height) {
+        var popupWidth = width || 640;
+        var popupHeight = height || 320;
+       
+        var windowLeft = window.screenLeft || window.screenX;
+        var windowTop = window.screenTop || window.screenY;
+    
+        var windowWidth = window.innerWidth || document.documentElement.clientWidth;
+        var WindowHeight = window.innerHeight || document.documentElement.clientHeight;
+    
+        var popupLeft = windowLeft + windowWidth / 2 - popupWidth / 2;
+        var popupTop = windowTop + WindowHeight / 2 - popupHeight / 2;
+    
+        var popup = window.open(url, title, 'scrollbars=yes, width=' + popupWidth + ', height=' + popupHeight + ', top=' + popupTop + ', left=' + popupLeft + ' ');
+        popup.focus();
+
+        return true;
+    };
+    
+    // Twitter
+    document.querySelector('.share_twitter').addEventListener('click', function(e) {
+        e.preventDefault();
+    
+        // Lien de l'url
+        var url = this.getAttribute('data-url');
+    
+        // Lien de partage
+        var shareUrl = "https://twitter.com/intent/tweet?text=" + encodeURIComponent(document.title) + 
+            "&via=Rapidomaine" + 
+            "&url=" + encodeURIComponent(url);
+    
+            popupCenter(shareUrl, "Partager sur Twitter");
+    }); 
+    
+    // Facebook 
+    document.querySelector('.share_facebook').addEventListener('click', function(e) {
+        e.preventDefault();
+    
+        var url = this.getAttribute('data-url');
+    
+        var shareUrl = "https://www.facebook.com/sharer/sharer.php?u=https://www.rapidomaine.fr/" + encodeURIComponent(document.title) + 
+            "&via=Rapidomaine" + 
+            "&url=" + encodeURIComponent(url);
+    
+        popupCenter(shareUrl, "Partager sur facebook");
+    }); 
+    
+    // LinkeDin
+    document.querySelector('.share_linkedin').addEventListener('click', function(e) {
+        e.preventDefault();
+    
+        var url = this.getAttribute('data-url');
+    
+        var shareUrl = "https://www.linkedin.com/shareArticle?url" + encodeURIComponent(document.title) + 
+            "&via=Rapidomaine" + 
+            "&url=" + encodeURIComponent(url);
+    
+        popupCenter(shareUrl, "Partager sur Linkedin");
+    }); 
+})()
